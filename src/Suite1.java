331d92f55bc2d1ff87de47614e74e1168c7af3f3
SebPou <sebastien.poussard@protonmail.ch>
2018-05-17 15:58:30 +0200

    premier essais jeudi

--- src/Suite1.java
+++ src/Suite1.java
@@ -1,19 +1,42 @@
+import java.math.BigInteger;
 
 public class Suite1 {
 	
 	public static void main(String [] args) {
-		double x0 =1;
-		double a =314125421;
-		double b =1;
-		System.out.println( gener(x0, a, b));
+		long x0 =43278;
+		long a =2;
+		long b =1;
+		int moyenne = 0;
+		for ( int i = 0 ; i < 100 ; i++) {
+			x0 = gener(x0, a, b);
+			
+			
+			//System.out.println(x0);
+			//System.out.println(calculNb1(x0));
+			System.out.printf("%027d \n", new BigInteger(Long.toBinaryString((long)x0)));
+			
+			moyenne += calculNb1(x0);
+			
+			//System.out.printf("%32d",d);
+
+		}
+		System.out.println(moyenne/100);
 	}
 
-	public static double gener(double x0, double a, double b) {
-		double m = Math.pow(10, 8);
-		double x = x0;
-		for( double i = 0; i < m ; i++ ) {
-			x = a*x+b;
+	public static long gener(long x0, long a, long b) {
+		long m = (long) Math.pow(10, 8);
+		long x = x0;
+		
+		return (a*x + b)%m;
+	}
+
+	public static int calculNb1(long x0) {
+		String x = Long.toBinaryString(x0);
+		int j =0;
+		for(int i = 0; i < x.length(); i++ ) {
+			if(x.charAt(i) == '1')
+				j++;
 		}
-		return x;
+		return j;
 	}
 }
