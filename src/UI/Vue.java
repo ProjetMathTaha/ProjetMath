a21cb7a2c5415f3da1a94d328a8ab20c4d62c85f
Digo <romero.diego31@gmail.com>
2018-05-24 15:26:35 +0200

    données + graph

--- src/UI/Vue.java
+++ src/UI/Vue.java
@@ -3,8 +3,10 @@ package UI;
 import java.awt.Color;
 import java.awt.Component; 
 import java.awt.FlowLayout; 
-import java.awt.GridLayout; 
- 
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel; 
 import javax.swing.JPanel; 
 import javax.swing.JTextField;
@@ -21,44 +23,70 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class Vue extends JPanel { 
  
   public Vue() { 
-    JPanel don = new JPanel(); 
-    don.setLayout(new GridLayout(3,1)); 
-     
-    don.add(new JLabel("A :")); 
-    JTextField donA = new JTextField(); 
-    don.add(donA); 
-    don.add(new JLabel("B :")); 
-    JTextField donB = new JTextField(); 
-    don.add(donB); 
-    don.add(new JLabel("X :")); 
-    JTextField donX = new JTextField(); 
-    don.add(donX); 
- 
-    this.add(don); 
-    
-    // Create dataset
-    XYDataset dataset = createDataset();
+	  
+	  
+	  	Controleur controleur = new Controleur(this);
+	    
+	  	JPanel don = new JPanel(); 
+	    this.add(don); 
+	    
+	    don.setLayout(new GridLayout(2,1));
+	    JPanel donHaut = new JPanel();
+	    donHaut.setLayout(new GridLayout(1,4)); 
+	     
+	    donHaut.add(new JLabel("A :")); 
+	    JTextField donA = new JTextField(); 
+	    //donA.setPreferredSize( new Dimension( 50, 0 ) );
 
-    // Create chart
-    JFreeChart chart = ChartFactory.createScatterPlot(
-        "Boys VS Girls weight comparison chart", 
-        "X-Axis", "Y-Axis", dataset);
+	    donHaut.add(donA); 
+	    donHaut.add(new JLabel("B :")); 
+	    JTextField donB = new JTextField(); 
+	    donHaut.add(donB); 
+	    donHaut.add(new JLabel("x0 :")); 
+	    JTextField donX = new JTextField(); 
 
-  //Changes background color
-    XYPlot plot = (XYPlot)chart.getPlot();
-    plot.setBackgroundPaint(new Color(255,228,196));
-    
-    ChartPanel panel = new ChartPanel(chart);
-    
-    
-    this.add(panel);
+	    donHaut.add(donX); 
+	    donHaut.add(new JLabel("m :"));
+	    JTextField donM = new JTextField(); 
+	    donHaut.add(donM);
+	    JButton valider = new JButton("valider");
+	    donHaut.add(valider);
+	    valider.addActionListener( controleur);
+	    
+	    
+	    don.add(donHaut);
+	    
+	    JLabel donBas = new JLabel();
+	     don.add(donBas);
+	    
+	    
+	    don.add(donX); 
+	 
+	    this.add(don); 
+	
+	
+	    
+	    // Create dataset
+	    XYDataset dataset = createDataset();
+	
+	    // Create chart
+	    JFreeChart chart = ChartFactory.createScatterPlot(
+	        "Nuage de points", 
+	        "Xn", "Xn+1", dataset);
+	
+	  //Changes background color
+	    XYPlot plot = (XYPlot)chart.getPlot();
+	    plot.setBackgroundPaint(new Color(255,228,196));
+	    
+	    ChartPanel panel = new ChartPanel(chart);
+	    this.add(panel);
   } 
 
   private XYDataset createDataset() {
 	    XYSeriesCollection dataset = new XYSeriesCollection();
 
 	    //Boys (Age,weight) series
-	    XYSeries series1 = new XYSeries("Boys");
+	    XYSeries series1 = new XYSeries("X0");
 	    series1.add(1, 72.9);
 	    series1.add(2, 81.6);
 	    series1.add(3, 88.9);
@@ -72,20 +100,8 @@ public class Vue extends JPanel {
 
 	    dataset.addSeries(series1);
 	    
-	   //Girls (Age,weight) series
-	    XYSeries series2 = new XYSeries("Girls");
-	    series2.add(1, 72.5);
-	    series2.add(2, 80.1);
-	    series2.add(3, 87.2);
-	    series2.add(4, 94.5);
-	    series2.add(5, 101.4);
-	    series2.add(6, 107.4);
-	    series2.add(7, 112.8);
-	    series2.add(8, 118.2);
-	    series2.add(9, 122.9);
-	    series2.add(10, 123.4);
+	  
 
-	    dataset.addSeries(series2);
 
 	    return dataset;
 	  }
