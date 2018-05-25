f87eb4f6cba35fde8fca5a43b48b2a676b96a570
SebPou <sebastien.poussard@protonmail.ch>
2018-05-25 14:04:51 +0200

    Refactoring du code de Diego
    lisibilité, attributs et commentaires

---
+++ src/Controleur.java
@@ -0,0 +1,7 @@
+import javax.swing.JPanel;
+
+public class Controleur {
+
+	public Controleur(JPanel j) {
+	}
+}
--- src/UI/Vue.java
+++ src/UI/Vue.java
@@ -1,69 +1,87 @@
 package UI;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component; 
-import java.awt.FlowLayout; 
 import java.awt.GridLayout;
-import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
-import javax.swing.JLabel; 
-import javax.swing.JPanel; 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection; 
  
 public class Vue extends JPanel { 
+	
+	private Map<String, JTextField> champsParametres; // contient les JTextField des parametres
+	private JButton valider;
  
   public Vue() { 
 	  
+	  	// instantiation des attributs de la classe
+	  	this.champsParametres = new HashMap<>();
 	  
-	  	Controleur controleur = new Controleur(this);
-	    
-	  	JPanel don = new JPanel(); 
-	    this.add(don); 
-	    
-	    don.setLayout(new GridLayout(2,1));
-	    JPanel donHaut = new JPanel();
-	    donHaut.setLayout(new GridLayout(1,4)); 
-	     
-	    donHaut.add(new JLabel("A :")); 
-	    JTextField donA = new JTextField(); 
-	    //donA.setPreferredSize( new Dimension( 50, 0 ) );
-
-	    donHaut.add(donA); 
-	    donHaut.add(new JLabel("B :")); 
-	    JTextField donB = new JTextField(); 
-	    donHaut.add(donB); 
-	    donHaut.add(new JLabel("x0 :")); 
-	    JTextField donX = new JTextField(); 
-
-	    donHaut.add(donX); 
-	    donHaut.add(new JLabel("m :"));
-	    JTextField donM = new JTextField(); 
-	    donHaut.add(donM);
-	    JButton valider = new JButton("valider");
-	    donHaut.add(valider);
-	    valider.addActionListener( controleur);
-	    
-	    
-	    don.add(donHaut);
-	    
-	    JLabel donBas = new JLabel();
-	     don.add(donBas);
-	    
-	    
-	    don.add(donX); 
-	 
-	    this.add(don); 
-	
+	  	// creation de 3 panels et ajout du layout a la classe
+	    this.setLayout(new BorderLayout());
+	  	JPanel parametres = new JPanel(); // contient les parametres à entrer par l'utilisateur
+	  	JPanel graphs = new JPanel(); // contient les graphs
+	  	JPanel stats = new JPanel(); // contient les stats
+	  	
+	  	this.add(parametres, BorderLayout.NORTH);
+	  	this.add(graphs, BorderLayout.CENTER);
+	  	this.add(stats, BorderLayout.SOUTH);
+	  	
+	  	// panel des parametres
+	  	parametres.setLayout(new GridLayout(2, 1));
+	  		// panel qui contient les textfield pour entrer les valeurs
+	  		JPanel parametresValeurs = new JPanel();
+	  		parametres.add(parametresValeurs);
+	  		parametresValeurs.setLayout(new GridLayout(1, 10));
+	  		
+	  		JLabel jlabelA = new JLabel("a :");
+	  		jlabelA.setHorizontalAlignment(SwingConstants.CENTER);
+	  		parametresValeurs.add(jlabelA);
+	  		champsParametres.put("a", new JTextField());
+	  		parametresValeurs.add(champsParametres.get("a"));
+	  		
+	  		JLabel jlabelB = new JLabel("b :");
+	  		jlabelB.setHorizontalAlignment(SwingConstants.CENTER);
+	  		parametresValeurs.add(jlabelB);
+	  		champsParametres.put("b", new JTextField());
+	  		parametresValeurs.add(champsParametres.get("b"));
+	  		
+	  		JLabel jlabelX0 = new JLabel("x0 :");
+	  		jlabelX0.setHorizontalAlignment(SwingConstants.CENTER);
+	  		parametresValeurs.add(jlabelX0);
+	  		champsParametres.put("x0", new JTextField());
+	  		parametresValeurs.add(champsParametres.get("x0"));
+	  		
+	  		JLabel jlabelM = new JLabel("m :");
+	  		jlabelM.setHorizontalAlignment(SwingConstants.CENTER);
+	  		parametresValeurs.add(jlabelM);
+	  		champsParametres.put("m", new JTextField());
+	  		parametresValeurs.add(champsParametres.get("m"));
+	  		
+	  		JLabel jlabelNB = new JLabel("nb :");
+	  		jlabelNB.setHorizontalAlignment(SwingConstants.CENTER);
+	  		parametresValeurs.add(jlabelNB);
+	  		champsParametres.put("nb", new JTextField());
+	  		parametresValeurs.add(champsParametres.get("nb"));
+	  		
+	  		
+	  		
+	  		// le bouton valider
+	  		this.valider = new JButton("valider");
+	  		parametres.add(this.valider);
 	
 	    
 	    // Create dataset
